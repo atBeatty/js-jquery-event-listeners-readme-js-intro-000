d355a2d4fe4e1e4b53bc8d8b44e577b554f131b5
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-21 03:03:38 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,13 +3,13 @@
 function getIt() {
   $("p").on('click', function() {
     alert('Hey!')
-  })
+  });
 }
 
 function frameIt() {
   $("img").on('load', function() {
     $("img").addClass('tasty')
-  })
+  });
 }
 
 function pressIt() {
