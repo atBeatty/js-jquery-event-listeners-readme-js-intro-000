a8908b6a8d0aef065f881a4dc6a9b2766fa82c8f
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-21 03:15:39 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt() {
 
 function pressIt() {
   $("form").on('keydown', function(key) {
-    if($("input") === 'G') {
+    if($("input:first").val() === 'G') {
       alert ('G was pressed')
     }
   })
