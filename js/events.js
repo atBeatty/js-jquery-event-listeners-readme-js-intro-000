71d92cdb730d6d3e93db8920a688eb7409c6c8e7
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-21 03:24:43 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,7 +15,7 @@ function frameIt() {
 function pressIt() {
   $("input").on('keydown', function(key) {
     if(key.which === '71') {
-      return alert ('G was pressed')
+      alert ('G was pressed')
     }
   });
 }
